3eddc4f6f05ddaf04835b00bd1a479e051b44ae3
NataliaNNH <91895418+NataliaNNH@users.noreply.github.com>
2021-11-07 19:05:21 +0100

    Create 20010.cpp

---
+++ src/20010.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main()
+{
+    int x, silnia =1, i=1;
+    std::cout<<"Podaj liczbe = ";
+    std::cin >>x;
+    do {
+       silnia*=i;
+       i++;
+    } while (i<=x);
+    std::cout<<"Silnia z liczby "<<x<<" wynosi = "<<silnia<<std::endl;
+    return 0;
+}
