c0f8366ec0392bb90dcba48bad2573715944b577
NataliaNNH <91895418+NataliaNNH@users.noreply.github.com>
2021-12-05 14:02:40 +0100

    Create 31.cpp

---
+++ src/31.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std; 
+auto x(int a[], int n) -> void
+{
+    for (int i =0; i<n; i++)
+    {
+        a[i] = 0;
+    }
+}
+int main(){
+
+    int tabl[100];
+    int n;
+    cout << "Podaj liczbe ";
+    cin >> n;
+   x(tabl, n);
+    for (int i = 0; i<n;i++)
+    {
+        cout << tabl[i] <<' ';
+    }
+    return 0;
+}
