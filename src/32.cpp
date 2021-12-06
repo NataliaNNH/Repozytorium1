14207c5f41f5a7ddd1e47b791c692ed8ceb17ec0
NataliaNNH <91895418+NataliaNNH@users.noreply.github.com>
2021-12-06 17:18:36 +0100

    Update 32.cpp

--- src/32.cpp
+++ src/32.cpp
@@ -1,21 +1,43 @@
 #include <iostream>
+#include <iostream>
 #include <numeric>
-using namespace std; 
-
-int main ()
+#include <string>
+using namespace std;
+auto x(string tekst) -> int
 {
     int n;
-    cout<<"Podaj liczbe ";
-    cin>>n;
-    int a[n];
-    int start;
-    cout<<"Od jakiej liczby chcesz zaczac?";
-    cin>>start;
-    iota (a, a+10,start);
-    cout<<"numery ";
-    for (int i:a) 
-    cout<< ' ' << i;
-    cout<<endl;
-    
+    cout << tekst << "\n";
+    cin >> n;
+    return n;
+}
+auto init(int a[], int n) -> void
+{
+    for (int i =0; i <n; i++)
+    {
+        a[i]=0;
+    }
+}
+auto iota(int a[], int n, int start) -> void
+{
+    for (int i=0; i <=n; i++)
+    {
+        a[i] = start;
+        start ++;
+    }
+}
+
+auto main () -> int
+{
+   int n = x("Podaj liczbe ");
+   int p = x("Od jakiej liczby chcesz zaczac?");
+   int a [100];
+   int i =0;
+   
+   iota (a, n, p);
+   while (i<n)
+   {
+       cout << a[i] << "\n";
+       i++;
+   }
     return 0;
 }
