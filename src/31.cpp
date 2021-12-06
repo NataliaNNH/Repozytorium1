5eb5d2ddf02ed1c33860ed92a5a4684c1cd8f14b
NataliaNNH <91895418+NataliaNNH@users.noreply.github.com>
2021-12-06 15:49:55 +0100

    Update 31.cpp

--- src/31.cpp
+++ src/31.cpp
@@ -2,18 +2,18 @@
 using namespace std; 
 auto x(int a[], int n) -> void
 {
-    for (int i =0; i<n; i++)
+    for (int i=0; i<n; i++)
     {
         a[i] = 0;
     }
 }
-int main(){
-
+int main()
+{
     int tabl[100];
     int n;
     cout << "Podaj liczbe ";
     cin >> n;
-   x(tabl, n);
+    x(tabl, n);
     for (int i = 0; i<n;i++)
     {
         cout << tabl[i] <<' ';
