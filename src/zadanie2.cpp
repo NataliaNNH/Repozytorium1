074ff45892f3cce2b3a19107e829518d04d2e79c
NataliaNNH <91895418+NataliaNNH@users.noreply.github.com>
2021-12-13 16:09:41 +0100

    Create zadanie2.cpp

---
+++ src/zadanie2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string Imie;
+	double Waga;
+	int Wzrost;
+	std::cout<<"Imie|Waga|Wzrost|"<<std::endl;
+	std::cin>>Imie>>Waga>>Wzrost;
+	std::cout<<"Imie: "<<Imie<<std::endl<<std::endl;
+	std::cout<<"Waga: "<<Waga<<"kg"<<std::endl<<std::endl;
+	std::cout<<"Wzrost: "<<Wzrost<<"cm"<<std::endl<<std::endl;
+return 0;
+}
